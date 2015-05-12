37f42ff4c6c3597b97cb41af181e93189adf70e7
Dom Barnes <dom@dombarnes.com>
2015-05-12 22:05:06 +0100

    Updating readme

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
-dotruby
-=======
+# dotruby
 
-Simple script to iterate through projects folder to list all .ruby_version settings
+Output a list of all versions of Ruby being used in subdirectories based on .ruby-version files used by [rbenv](http://github.com/sstephenson/rbenv).
 
+## Installation
+If you already have ~/bin in your $PATH:
+
+> mv dotruby.rb ~/bin/dotruby  
+> chmod +x ~/bin/dotruby  
+
+Then reload your shell
+
+## Usage
+Run this in your projects folder:  
+> $ cd ~/projects  
+> $ dotruby
+-- Checking /Users/domster83/projects --
+> chatty ~> 2.1.2  
+> internalapp ~> 2.1.2  
+> knut ~> 2.1.2  
+> Leave-Management-System ~> 1.9.3-p448  
+> PunchClockServer ~> 2.0.0-p353  
+> RescueBoard ~> 2.2.1  
+> Slogger ~> 2.1.2  
+> StatusBoard-Panic ~> test  
+> team_dashboard ~> 2.1.2  
+> toggl_status_board ~> 1.9.3-p362  
--- dotruby.rb
+++ dotruby.rb
@@ -1,27 +1,45 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby
 # Author: Dom Barnes
 # Web: dombarnes.com
 # This script will iterate through your projectDir finding all instances of .ruby-version and writing them to ruby apps.txt
 # I wrote this so I know which projects need ot be updated when a new version of ruby is released.
 
-homeDir = File.expand_path(ENV['HOME'])
-# set your porject folder relative to ~/
-projectDir = homeDir+ "/projects/"
-rubyList = File.open(homeDir + "/projects/ruby\ apps.txt", "w")
+# case ARGV[0]
+# homeDir = File.expand_path(ENV['HOME'])
+# require 'optparse'
 
-	Dir.foreach(projectDir) do |item|
-		if File.directory?(item)
-			next if item == '.' or item == '..'
-			if File.file?(projectDir + item + '/.ruby-version')
-				data = File.read(projectDir + '/' + item + '/.ruby-version')
-				puts data
-				rubyList.write("/" + item + "\t" + data)
-			else
-				next
-			end
+# options = {}
+# OptionParser.new do |opts|
+# 	opts.banner = "Usage dotruby.rb [options]"
+
+# 	opts.on("-p", "--path", "Set your projects path") do |p|
+# 		options[:path] = v
+# 	end
+# end.parse!
+puts "path is " + ARVG[0]
+if ARGV[0]?
+	searchDir = File.expand_path(ARGV[0])
+else
+	searchDir = File.expand_path(ENV['HOME'])
+end
+
+puts searchDir
+
+puts "Checking #{searchDir}"
+# rubyList = File.new(searchDir + "/ruby-apps.txt", "w")
+
+Dir.foreach(searchDir) do |item|
+	if File.directory?(item)
+		next if item == '.' or item == '..' or item == '.DS_Store'
+		if File.file?(searchDir + '/' + item + '/.ruby-version')
+			data = File.read(searchDir + '/' + item + '/.ruby-version')
+			puts item + " =>" + data
+			# rubyList.write("/" + item + "\t" + data)
 		else
 			next
 		end
+	else
+		next
 	end
-# end
+end
 rubyList.close
