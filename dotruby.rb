438f630e990d9958a9b132ffdbaf427a2febf537
Dom Barnes <dom@dombarnes.com>
2016-01-03 22:56:47 +0000

    trying to add argument support

--- dotruby.rb
+++ dotruby.rb
@@ -4,19 +4,33 @@
 # This script will iterate through your projectDir finding all instances of .ruby-version and writing them to ruby apps.txt
 # I wrote this so I know which projects need ot be updated when a new version of ruby is released.
 
-searchDir = Dir.pwd
-puts "Checking #{searchDir}"
+def colorize(text, color_code)
+  "\e[#{color_code}m#{text}\e[0m"
+end
+
+def red(text); colorize(text, 31); end
+def green(text); colorize(text, 32); end
+def normal(text); colorize(text, 1); end
+
+searchDir = ARGV[0] || Dir.pwd
+puts green("Checking #{searchDir}")
+count = 0
 
 Dir.foreach (searchDir) do |item|
 	if File.directory?(item)
 		next if item == '.' or item == '..' or item == '.DS_Store'
-		if File.file?(searchDir + '/' + item + '/.ruby-version')
+		if File.exists?(searchDir + '/' + item + '/.ruby-version')
 			data = File.read(searchDir + '/' + item + '/.ruby-version')
-			puts item + " =>" + data
+			puts "#{normal(item)}: #{green(data)}"
+			count += 1
 		else
 			next
 		end
 	else
 		next
 	end
+end
+
+if count == 0 
+	puts "#{red("You haven't set ant ruby versions")}"
 end
